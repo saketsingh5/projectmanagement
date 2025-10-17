caaa874748f030920d9eb7a02e993a9becd84195
Saket Singh <saket3015@gmail.com>
2025-10-17 22:20:46 +0530

    async handler and routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 
 //base configuration
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended:true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 //cors config
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-    credentials:true,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
+// import routes
+import healthCheckRouter from "./routes/healthCheck.routes.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
-export default app
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import dotenv from "dotenv"
-import app from "./app.js"
+import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/connnection.js";
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
 const port = process.env.PORT || 3000;
 
 connectDB()
-    .then(()=>{
-      app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-      })
-    })
-    .catch((err)=>{
-      console.error("DB connection Failed", err)
-    })
-
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection Failed", err);
+  });
